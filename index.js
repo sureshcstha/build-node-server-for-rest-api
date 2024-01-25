29f0440c5bd0cb44f84017d2ca951f53d05c6a4f
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-24 21:49:31 -0700

    read pie data from a file

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ let pieRepo = require('./repos/pieRepo');
 // Use the express Router object
 let router = express.Router();
 
-// Get pie data synchronously
-let pies = pieRepo.get();
-
 // Create GET to return a list of all pies
 router.get('/', function (req, res, next) {
-    res.status(200).json({
-        "status": 200,
-        "statusText": "OK",
-        "message": "All pies retrieved.",
-        "data": pies
+    pieRepo.get(function (data) {
+        res.status(200).json({
+          "status": 200,
+          "statusText": "OK",
+          "message": "All pies retrieved.",
+          "data": data
+        });
+    }, function (err) {
+        next(err);
     });
 });
 
--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -1,11 +1,18 @@
+let fs = require('fs'); // fs is built in node module that know how to work with reading and writing files
+
+const FILE_NAME = './assets/pies.json';
+
 let pieRepo = {
-    get: function() {
-      return [
-        { "id": 1, "name": "Apple" },
-        { "id": 2, "name": "Cherry" },
-        { "id": 3, "name": "Peach" }
-      ];
-    }
+    get: function (resolve, reject) {
+        fs.readFile(FILE_NAME, function (err, data) {
+          if (err) {
+            reject(err);
+          }
+          else {
+            resolve(JSON.parse(data));
+          }
+        });
+    },
 };
   
 module.exports = pieRepo;
