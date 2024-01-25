edb3cf613ef8657a34491a6d088d9e8d6dc9c9ca
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-24 23:12:06 -0700

    write errors to a log file

--- helpers/errorHelpers.js
+++ helpers/errorHelpers.js
@@ -1,8 +1,24 @@
+const logRepo = require("../repos/logRepo");
+
 let errorHelpers = {
     logErrorsToConsole: function (err, req, res, next) {
       console.error("Log Entry: " + JSON.stringify(errorHelpers.errorBuilder(err)));
       console.error("*".repeat(80));
       next(err)
+    },
+    logErrorsToFile: function (err, req, res, next) {
+        let errorObject = errorHelpers.errorBuilder(err);
+        errorObject.requestInfo = {
+          "hostname": req.hostname,
+          "path": req.path,
+          "app": req.app,
+        }
+        logRepo.write(errorObject, function (data) {
+          console.log(data);
+        }, function (err) {
+          console.error(err);
+        });
+        next(err)
     },
     clientErrorHandler: function (err, req, res, next) {
       if (req.xhr) {
--- index.js
+++ index.js
@@ -180,6 +180,8 @@ app.use('/api/', router);
 
 // Configure exception logger to console
 app.use(errorHelper.logErrorsToConsole);
+// Configure exception logger to file
+app.use(errorHelper.logErrorsToFile);
 // Configure client error handler
 app.use(errorHelper.clientErrorHandler);
 // Configure catch-all exception middleware last
